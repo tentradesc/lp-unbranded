b8d495cf2c8a7c5a88ce9b1a138dc8f76df0b05e
Andy Cholakov <andy@acdigital.agency>
2025-05-28 14:20:35 +0300

    jQuery import

--- head.php
+++ head.php
@@ -1,4 +1,5 @@
 <?php include __DIR__ . '/gtm_head.php'; ?>
 <link href="https://tradeinfuture.com/lp/v2/includes/toastr.min.css" rel="stylesheet"/>
 <script src="https://tradeinfuture.com/lp/v2/includes/toastr.min.js"></script>
+<script src="https://tradeinfuture.com/lp/v2/includes/jquery-3.7.1.min.js"></script>
 <?= $headExtras ?? '' ?>
