89da47d5c7b9035129c902e8847163d2bac1f361
Andy Cholakov <andy@acdigital.agency>
2025-05-28 14:16:12 +0300

    Toaster initial commit

--- head.php
+++ head.php
@@ -1,2 +1,4 @@
 <?php include __DIR__ . '/gtm_head.php'; ?>
+<link href="https://tradeinfuture.com/lp/v2/includes/toastr.min.css" rel="stylesheet"/>
+<script src="https://tradeinfuture.com/lp/v2/includes/toastr.min.js"></script>
 <?= $headExtras ?? '' ?>
